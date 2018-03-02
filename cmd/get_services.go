340a6412b4b6b1fa57ec76bba8f99f5004bdabec
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2018-03-02 12:04:26 +0900

    Show ECS service specified cluster

--- cmd/get_services.go
+++ cmd/get_services.go
@@ -23,6 +23,10 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/spf13/cobra"
 )
 
@@ -33,8 +37,48 @@ var servicesCmd = &cobra.Command{
 	Use:   "services",
 	Short: "get all ECS services specified cluster",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("get all services of %s\n", cluster)
+		listServices()
 	},
+}
+
+func listServices() {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic("failed to load config, " + err.Error())
+	}
+	svc := ecs.New(cfg)
+	input := &ecs.ListServicesInput{
+		Cluster: aws.String(cluster),
+	}
+
+	req := svc.ListServicesRequest(input)
+	result, err := req.Send()
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecs.ErrCodeServerException:
+				fmt.Println(ecs.ErrCodeServerException, aerr.Error())
+			case ecs.ErrCodeClientException:
+				fmt.Println(ecs.ErrCodeClientException, aerr.Error())
+			case ecs.ErrCodeInvalidParameterException:
+				fmt.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+			case ecs.ErrCodeClusterNotFoundException:
+				fmt.Println(ecs.ErrCodeClusterNotFoundException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result.NextToken)
+	for _, arn := range result.ServiceArns {
+		fmt.Println(arn)
+	}
 }
 
 func init() {
