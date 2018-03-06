7f661c6be0a09fec9c6a66619c22059345da5035
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2018-03-07 00:01:35 +0900

    Added prompt ui example

--- cmd/walk.go
+++ cmd/walk.go
@@ -23,6 +23,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -31,7 +32,20 @@ var walkCmd = &cobra.Command{
 	Use:   "walk",
 	Short: "describe ECS services by selecting cluster interactively",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("walk called")
+		prompt := promptui.Select{
+			Label: "Select Day",
+			Items: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+				"Saturday", "Sunday"},
+		}
+
+		_, result, err := prompt.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+
+		fmt.Printf("You choose %q\n", result)
 	},
 }
 
