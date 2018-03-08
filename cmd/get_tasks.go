9746f008e75d8f3b98c1b220bb0956421b0a3bee
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2018-03-09 01:10:33 +0900

    Added template get tasks cmd

---
+++ cmd/get_tasks.go
@@ -0,0 +1,57 @@
+// Copyright © 2018 Masato Oshima
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var getTasksCmdFlagCluster string
+var getTasksCmdFlagService string
+
+// tasksCmd represents the tasks command
+var getTasksCmd = &cobra.Command{
+	Use:   "tasks",
+	Short: "get Tasks specified service",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("tasks called")
+	},
+}
+
+func init() {
+	getCmd.AddCommand(getTasksCmd)
+	getTasksCmd.Flags().StringVarP(&getTasksCmdFlagCluster, "cluster", "c", "", "AWS ECS cluster)")
+	getTasksCmd.MarkFlagRequired("cluster")
+	getTasksCmd.Flags().StringVarP(&getTasksCmdFlagService, "service", "s", "", "AWS ECS cluster)")
+	getTasksCmd.MarkFlagRequired("service")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// tasksCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// tasksCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
